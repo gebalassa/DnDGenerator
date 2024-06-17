bf2235c6ad71915c6cd213e64202bbc8a27946f6
gebalassa <luciuswrath@hotmail.com>
2024-06-17 09:58:56 -0400

    Inicio WFCManager.cs

---
+++ Assets/GaboScripts/WFC/WFCManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WFCManager : MonoBehaviour
+{
+    //TODO: TERMINARRRRR
+    public GridClass GetWFC(GridClass grid)
+    {
+        GridClass newGrid = new GridClass(grid.width, grid.height);
+
+        //CODIGOOOOO
+
+        return newGrid;
+    }
+}
